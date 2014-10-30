6ee6e370cbe8c5ea129f86166a5dae461f787371
zcsminifast <chongshanzhang1988@163.com>
2014-10-30 15:43:03 +0800

    添加基础报表网页

---
+++ StatisticalReport/StatisticalReport.Web/UI_StatisticalReport/VBMonthly/table_ClinkerMonthlyOutput.aspx.cs
@@ -0,0 +1,69 @@
+﻿using StatisticalReport.Service.StatisticalReportServices;
+using StatisticalReport.Service.StatisticalReportServices.Daily;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StatisticalReport.Web.UI_StatisticalReport.VBMonthly
+{
+    public partial class table_ClinkerMonthlyOutput : WebStyleBaseForEnergy.webStyleBase
+    {
+        private const string REPORT_TEMPLATE_PATH = "\\VBReportHeaderTemplate\\table_ClinkerMonthlyOutput.xml";
+        private static DataTable myDataTable;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            base.InitComponts();
+
+            if (!IsPostBack)
+            {
+
+            }
+
+            ///以下是接收js脚本中post过来的参数
+            string m_FunctionName = Request.Form["myFunctionName"] == null ? "" : Request.Form["myFunctionName"].ToString();             //方法名称,调用后台不同的方法
+            string m_Parameter1 = Request.Form["myParameter1"] == null ? "" : Request.Form["myParameter1"].ToString();                   //方法的参数名称1
+            string m_Parameter2 = Request.Form["myParameter2"] == null ? "" : Request.Form["myParameter2"].ToString();                   //方法的参数名称2
+            if (m_FunctionName == "ExcelStream")
+            {
+                //ExportFile("xls", "导出报表1.xls");
+                string[] m_TagData = new string[] { "10月份", "报表类型:日报表", "汇总人:某某某", "审批人:某某某" };
+                string m_HtmlData = StatisticalReportHelper.CreateExportHtmlTable(mFileRootPath +
+                    REPORT_TEMPLATE_PATH, myDataTable, m_TagData);
+                StatisticalReportHelper.ExportExcelFile("xls", "熟料生产线产量及消耗统计月报表.xls", m_HtmlData);
+            }
+        }
+
+        /// <summary>
+        /// 获得报表数据并转换为json
+        /// </summary>
+        /// <returns>column的json字符串</returns>
+        [WebMethod]
+        public static string GetReportData(string organizationId, string datetime)
+        {
+            //myDataTable = ClinkerMonthlyPeakerValleyFlatElectricityConsumption.TableQuery("df863854-89ae-46e6-80e8-96f6db6471b4", "2014-10");
+            myDataTable = CementMilEnergyConsumption_TargetCompletion.TableQuery(organizationId, datetime);
+            string m_UserInfoJson = StatisticalReportHelper.ReadReportHeaderFile(mFileRootPath +
+                REPORT_TEMPLATE_PATH, myDataTable);
+            return m_UserInfoJson;
+        }
+
+        /// <summary>
+        /// 获得报表数据并转换为json
+        /// </summary>
+        /// <returns>column的json字符串</returns>
+        [WebMethod]
+        public static string PrintFile()
+        {
+            string[] m_TagData = new string[] { "10月份", "报表类型:日报表", "汇总人:某某某", "审批人:某某某" };
+            string m_HtmlData = StatisticalReportHelper.CreatePrintHtmlTable(mFileRootPath +
+                REPORT_TEMPLATE_PATH, myDataTable, m_TagData);
+            return m_HtmlData;
+        }
+    }
+}
---
+++ StatisticalReport/StatisticalReport.Web/UI_StatisticalReport/js/page/VBMonthly/table_FormulaMonth.js
@@ -0,0 +1,137 @@
+﻿$(function () {
+    //var m_UserName = $('#HiddenField_UserName').val();
+    //loadGridData('first');
+    InitializeGrid('');
+});
+
+function loadGridData(myLoadType, organizationId, datetime) {
+    //parent.$.messager.progress({ text: '数据加载中....' });
+    var m_MsgData;
+    $.ajax({
+        type: "POST",
+        url: "table_PublicFormulaMonth.aspx/GetReportData",
+        data: '{organizationId: "' + organizationId + '", datetime: "' + datetime + '"}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            if (myLoadType == 'first') {
+                m_MsgData = jQuery.parseJSON(msg.d);
+                InitializeGrid(m_MsgData);
+            }
+            else if (myLoadType == 'last') {
+                m_MsgData = jQuery.parseJSON(msg.d);
+                $('#gridMain_ReportTemplate').datagrid('loadData', m_MsgData['rows']);
+            }
+        }
+    });
+}
+function InitializeGrid(myData) {
+
+    $('#gridMain_ReportTemplate').datagrid({
+        title: '',
+        data: myData,
+        dataType: "json",
+        striped: true,
+        idField: "field",
+        //frozenColumns: [[m_IdAndNameColumn[1]]],
+        columns: myData['columns'],
+        //loadMsg: '',   //设置本身的提示消息为空 则就不会提示了的。这个设置很关键的
+        rownumbers: true,
+        //pagination: true,
+        singleSelect: true,
+        //onClickCell: onClickCell,
+        //idField: m_IdAndNameColumn[0].field,
+        //pageSize: 20,
+        //pageList: [20, 50, 100, 500],
+
+        toolbar: '#toolbar_ReportTemplate'
+    });
+
+    //for(
+}
+
+function ExportFileFun() {
+    var m_FunctionName = "ExcelStream";
+    var m_Parameter1 = "Parameter1";
+    var m_Parameter2 = "Parameter2";
+
+    var form = $("<form id = 'ExportFile'>");   //定义一个form表单
+    form.attr('style', 'display:none');   //在form表单中添加查询参数
+    form.attr('target', '');
+    form.attr('method', 'post');
+    form.attr('action', "table_PublicFormulaMonth.aspx");
+
+    var input_Method = $('<input>');
+    input_Method.attr('type', 'hidden');
+    input_Method.attr('name', 'myFunctionName');
+    input_Method.attr('value', m_FunctionName);
+    var input_Data1 = $('<input>');
+    input_Data1.attr('type', 'hidden');
+    input_Data1.attr('name', 'myParameter1');
+    input_Data1.attr('value', m_Parameter1);
+    var input_Data2 = $('<input>');
+    input_Data2.attr('type', 'hidden');
+    input_Data2.attr('name', 'myParameter2');
+    input_Data2.attr('value', m_Parameter2);
+
+    $('body').append(form);  //将表单放置在web中 
+    form.append(input_Method);   //将查询参数控件提交到表单上
+    form.append(input_Data1);   //将查询参数控件提交到表单上
+    form.append(input_Data2);   //将查询参数控件提交到表单上
+    form.submit();
+    //释放生成的资源
+    form.remove();
+
+    /*
+    var m_Parmaters = { "myFunctionName": m_FunctionName, "myParameter1": m_Parameter1, "myParameter2": m_Parameter2 };
+    $.ajax({
+        type: "POST",
+        url: "Report_Example.aspx",
+        data: m_Parmater,                       //'myFunctionName=' + m_FunctionName + '&myParameter1=' + m_Parameter1 + '&myParameter2=' + m_Parameter2,
+        //contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            if (msg.d == "1") {
+                alert("导出成功!");
+            }
+            else{
+                alert(msg.d);
+            }
+        }
+    });
+    */
+}
+function RefreshFun() {
+    loadGridData('last');
+}
+function PrintFileFun() {
+    $.ajax({
+        type: "POST",
+        url: "table_PublicFormulaMonth.aspx/PrintFile",
+        data: "",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            PrintHtml(msg.d);
+        }
+    });
+
+
+}
+
+function QueryReportFun() {
+    var organizationID = $('#organizationId').val();
+    var datetime = $('#datetime').datetimebox('getValue');
+    if (organizationID == "" || datetime == "") {
+        $.messager.alert('警告', '请选择生产线和时间');
+        return;
+    }
+
+    loadGridData('first', organizationID, datetime);
+}
+
+// datetime spinner
+function onOrganisationTreeClick(node) {
+    $('#productLineName').val(node.text);
+    $('#organizationId').val(node.OrganizationID);
+}
