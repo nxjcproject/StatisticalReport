ae4b9d0bda43642daf055f9602f3c01bec81ea79
shaofengzong <43986754+shaofengzong@users.noreply.github.com>
2019-05-10 15:58:25 +0800

    环保数据统计bug修正及甲乙丙数据算法修改
    主要更改甲乙丙数据算法，及合计算法

--- StatisticalReport/StatisticalReport.Service/BasicDataSummaryReport/EnvironmentalStatisticalReportService.cs
+++ StatisticalReport/StatisticalReport.Service/BasicDataSummaryReport/EnvironmentalStatisticalReportService.cs
@@ -8,19 +8,21 @@ using System.Linq;
 using System.Text;
 namespace StatisticalReport.Service.BasicDataSummaryReport
 {
-   public class EnvironmentalStatisticalReportService
-   {
-       /// <summary>
-       /// 查询所有产线的量
-       /// </summary>
-       /// <param name="organizationIds">组织机构ID数组（通常为授权的组织机构ID数组）</param>
-       /// <param name="time">时间</param>
-       /// <returns></returns>
-       public static DataTable GetEnvironmentalByOrganiztionIds(string organizationId, DateTime startDate, DateTime endDate)
-       {
-           string connectionString = ConnectionStringFactory.NXJCConnectionString;
-           ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
-           string queryString1 = @"SELECT X.CompanyName,X.FactoryName,X.ItemName,Y.FirstB,Y.SecondB,Y.ThirdB,Y.PeakB,Y.ValleyB,Y.FlatB,Y.A班,Y.B班,Y.C班,Y.D班,Y.TotalPeakValleyFlatB
+    public class EnvironmentalStatisticalReportService
+    {
+        public static int workingTeamNum = 4;//人员班默认ABCD  根据查询结果重新赋值
+        public static int shiftNum = 3;//工作组默认甲乙丙  根据查询结果重新赋值
+        /// <summary>
+        /// 查询所有产线的量
+        /// </summary>
+        /// <param name="organizationIds">组织机构ID数组（通常为授权的组织机构ID数组）</param>
+        /// <param name="time">时间</param>
+        /// <returns></returns>
+        public static DataTable GetEnvironmentalByOrganiztionIds(string organizationId, DateTime startDate, DateTime endDate)
+        {
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
+            string queryString1 = @"SELECT X.CompanyName,X.FactoryName,X.ItemName,Y.FirstB,Y.SecondB,Y.ThirdB,Y.PeakB,Y.ValleyB,Y.FlatB,Y.A班,Y.B班,Y.C班,Y.D班,Y.TotalPeakValleyFlatB
                                    FROM 
                                   ((SELECT C.Name as CompanyName,A.Name as FactoryName, B.OrganizationID,B.LevelCode,B.Type,B.LevelType,B.ENABLED,D.ItemName,D.VariableId
                                    FROM system_Organization A
@@ -53,127 +55,181 @@ namespace StatisticalReport.Service.BasicDataSummaryReport
 					                                    ON X.VariableId=Y.VariableId and X.OrganizationID=Y.OrganizationID) 
                                     ORDER BY X.LevelCode,X.ItemName
                                         ";
-           queryString1 = string.Format(queryString1, startDate.ToString("yyyy-MM-dd"), endDate.ToString("yyyy-MM-dd"), organizationId);
+            queryString1 = string.Format(queryString1, startDate.ToString("yyyy-MM-dd"), endDate.ToString("yyyy-MM-dd"), organizationId);
 
-           DataTable result1 = dataFactory.Query(queryString1);
-           DataTable result2 = GetModifyEnvironmental(result1, organizationId, startDate.ToString("yyyy-MM-dd"), endDate.ToString("yyyy-MM-dd"));
-           return result2;   
-       }
+            DataTable result1 = dataFactory.Query(queryString1);
+            DataTable result2 = GetModifyEnvironmental(result1, organizationId, startDate.ToString("yyyy-MM-dd"), endDate.ToString("yyyy-MM-dd"));
+            for (int i = 0; i < result2.Rows.Count; i++)
+            {
+                for (int j = 3; j < result2.Columns.Count; j++)
+                {
+                    if (Convert.ToDouble(result2.Rows[i][j]) == 0.00)
+                    {
+                        result2.Rows[i][j] = DBNull.Value;
+                    }
+                }
+            }
+
+            return result2;
+        }
 
 
-       public static DataTable GetShiftsSchedulingLogMonthly(string organizationId, string startDate, string endDate)
-       {
-           string connectionString = ConnectionStringFactory.NXJCConnectionString;
-           ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
+        public static DataTable GetShiftsSchedulingLogMonthly(string organizationId, string startDate, string endDate)
+        {
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
 
-           string sql = @" SELECT [TimeStamp],[FirstWorkingTeam],[SecondWorkingTeam],[ThirdWorkingTeam]
+            string sql = @" SELECT [TimeStamp],[FirstWorkingTeam],[SecondWorkingTeam],[ThirdWorkingTeam]
                               FROM [tz_Balance]
                               WHERE TimeStamp>=@startDate AND TimeStamp<=@endDate
 		                            and StaticsCycle = 'day' AND
 		                            [OrganizationID] = @organizationId
                               ORDER BY [TimeStamp]";
-           SqlParameter[] parameters = new SqlParameter[]{
+            SqlParameter[] parameters = new SqlParameter[]{
                 new SqlParameter("organizationId", organizationId),
                 new SqlParameter("startDate", startDate),
                 new SqlParameter("endDate",endDate)
             };
-           DataTable result2 = dataFactory.Query(sql, parameters);
-           return result2;
-       }
-       public static DataTable GetModifyEnvironmental(DataTable environmentalTable,string organizationId, string startDate, string endDate)
-       {
-           //不在采用旧的合计列算法（即利用TotalPeakVallyFlatB），改为甲乙丙班求和，除以3。
-           double sum = 0.0;
-           for (int i = 0; i < environmentalTable.Rows.Count; i++)
-           {
-               for (int j = 3; j < 6; j++)//3、4、5列对应甲乙丙班
-               {
-                   sum += Convert.ToDouble(environmentalTable.Rows[i][j]);
-               }
-               environmentalTable.Rows[i][environmentalTable.Columns.Count-1] = sum/3;
-               sum = 0.0;
-           }
+            DataTable result2 = dataFactory.Query(sql, parameters);
+            return result2;
+        }
+        public static DataTable GetModifyEnvironmental(DataTable environmentalTable, string organizationId, string startDate, string endDate)
+        {
+            for (int i = 0; i < environmentalTable.Rows.Count; i++)
+            {
+                for (int j = 3; j < environmentalTable.Columns.Count; j++)
+                {
+                    if (environmentalTable.Rows[i][j].ToString() == "")
+                    {
+                        environmentalTable.Rows[i][j] = 0.0;
+                    }
+                }
+            }
+            DataTable shiftTable = GetShiftsSchedulingLogMonthly(organizationId, startDate, endDate);
+            //===============判断查询区间内   排班情况中    夜白中。数量,放到int数组中
+            int[] firstSecondThirdTeamTotal = new int[shiftTable.Columns.Count - 1];
+            for (int i = 0; i < shiftTable.Rows.Count; i++)
+            {
+                for (int j = 1; j < shiftTable.Columns.Count; j++)
+                {
+                    if (shiftTable.Rows[i][j].ToString() != "")
+                    {
+                        firstSecondThirdTeamTotal[j - 1]++;
+                    }
+                }
+            }
+            //======================================================================
+            Dictionary<string, int> shiftDic = new Dictionary<string, int>();
+            shiftDic.Add("A班", 0);
+            shiftDic.Add("B班", 0);
+            shiftDic.Add("C班", 0);
+            shiftDic.Add("D班", 0);
+            for (int i = 0; i < shiftTable.Rows.Count; i++)
+            {
+                for (int j = 1; j < shiftTable.Columns.Count; j++)
+                {
+                    if (shiftTable.Rows[i][j].ToString() == "A班")
+                    {
+                        shiftDic["A班"] += 1;
+                    }
+                    else if (shiftTable.Rows[i][j].ToString() == "B班")
+                    {
+                        shiftDic["B班"] += 1;
+                    }
+                    else if (shiftTable.Rows[i][j].ToString() == "C班")
+                    {
+                        shiftDic["C班"] += 1;
+                    }
+                    else if (shiftTable.Rows[i][j].ToString() == "D班")
+                    {
+                        shiftDic["D班"] += 1;
+                    }
+                }
+            }
+            DateTime m_startDate = Convert.ToDateTime(startDate);
+            DateTime m_endDate = Convert.ToDateTime(endDate);
+            TimeSpan span = m_endDate - m_startDate;
+            int totalDays = span.Days + 1;
+            for (int i = 0; i < environmentalTable.Rows.Count; i++)
+            {
+                for (int j = 3; j < environmentalTable.Columns.Count; j++)
+                {
+                    if (j == 9)
+                    {
+                        if (shiftDic["A班"] != 0)
+                        {
+                            environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["A班"]);
+                        }
+                    }
+                    else if (j == 10)
+                    {
+                        if (shiftDic["B班"] != 0)
+                        {
+                            environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["B班"]);
+                        }
+                    }
+                    else if (j == 11)
+                    {
+                        if (shiftDic["C班"] != 0)
+                        {
+                            environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["C班"]);
+                        }
+                    }
+                    else if (j == 12)
+                    {
+                        if (shiftDic["D班"] != 0)
+                        {
+                            environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["D班"]);
+                        }
+                    }
+                    else if (j >= 3 && j <= 5)
+                    {
+                        if (firstSecondThirdTeamTotal[j-3] != 0)
+                        {
+                            environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / firstSecondThirdTeamTotal[j-3]);
+                        }
+                    }
+                    else
+                    {
+                        environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / totalDays);
+                    }
+                }
+            }
+            //不在采用旧的合计列算法（即利用TotalPeakVallyFlatB），改为甲乙丙班求和，除以3。若某一班是0则除以2
+            
+            double sum = 0.0;
+            int activeShiftNum = 0;
+            for (int i = 0; i < environmentalTable.Rows.Count; i++)
+            {
+                for (int j = 3; j < 6; j++)//3、4、5列对应甲乙丙班
+                {
+                    if (Convert.ToDouble(environmentalTable.Rows[i][j]) != 0.0)
+                    {
+                        sum += Convert.ToDouble(environmentalTable.Rows[i][j]);
+                        activeShiftNum++;
+                    }
+                }
+                if (activeShiftNum != 0)
+                {
+                    environmentalTable.Rows[i][environmentalTable.Columns.Count - 1] = sum / activeShiftNum;
+                }
+                else
+                {
+                    environmentalTable.Rows[i][environmentalTable.Columns.Count - 1] = 0.0;
+                }
+                sum = 0.0;
+                activeShiftNum = 0;
+            }
+            //===================================
+            return environmentalTable;
+        }
 
-           //===================================
-           DataTable shiftTable=GetShiftsSchedulingLogMonthly(organizationId, startDate, endDate);
-           Dictionary<string, int> shiftDic = new Dictionary<string, int>();
-           shiftDic.Add("A班", 0);
-           shiftDic.Add("B班", 0);
-           shiftDic.Add("C班", 0);
-           shiftDic.Add("D班", 0);
-           for (int i = 0; i < shiftTable.Rows.Count; i++)
-           {
-               for (int j = 1; j < shiftTable.Columns.Count; j++)
-               {
-                   if (shiftTable.Rows[i][j].ToString()=="A班")
-                   {
-                       shiftDic["A班"] += 1;
-                   }
-                   else if (shiftTable.Rows[i][j].ToString() == "B班")
-                   {
-                       shiftDic["B班"] += 1;
-                   }
-                   else if (shiftTable.Rows[i][j].ToString() == "C班")
-                   {
-                       shiftDic["C班"] += 1;
-                   }
-                   else if (shiftTable.Rows[i][j].ToString() == "D班")
-                   {
-                       shiftDic["D班"] += 1;
-                   }
-               }
-           }
-           DateTime m_startDate = Convert.ToDateTime(startDate);
-           DateTime m_endDate = Convert.ToDateTime(endDate);
-           TimeSpan span=m_endDate - m_startDate;
-           int totalDays=span.Days+1;
-           for (int i = 0; i < environmentalTable.Rows.Count; i++)
-           {
-               for (int j = 3; j < environmentalTable.Columns.Count; j++)
-               {
-                   if (j == 9)
-                   {
-                       if (shiftDic["A班"]!=0)
-                       {
-                           environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["A班"]);
-                       }
-                   }
-                   else if (j == 10)
-                   {
-                       if (shiftDic["B班"]!=0)
-                       {
-                           environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["B班"]);
-                       }
-                   }
-                   else if (j == 11)
-                   {
-                       if (shiftDic["C班"]!=0)
-                       {
-                           environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["C班"]);
-                       }
-                   }
-                   else if (j == 12)
-                   {
-                       if (shiftDic["D班"]!=0)
-                       {
-                           environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / shiftDic["D班"]);
-                       }
-                   }
-                   else
-                   {
-                       environmentalTable.Rows[i][j] = (Convert.ToDouble(environmentalTable.Rows[i][j]) / totalDays);
-                   }
-               }
-           }
-           return environmentalTable;
-       }
-
-       public static void ExportExcelFile(string myFileType, string myFileName, string myData)
-       {
-           if (myFileType == "xls")
-           {
-               UpDownLoadFiles.DownloadFile.ExportExcelFile(myFileName, myData);
-           }
-       }
-   }
+        public static void ExportExcelFile(string myFileType, string myFileName, string myData)
+        {
+            if (myFileType == "xls")
+            {
+                UpDownLoadFiles.DownloadFile.ExportExcelFile(myFileName, myData);
+            }
+        }
+    }
 }
